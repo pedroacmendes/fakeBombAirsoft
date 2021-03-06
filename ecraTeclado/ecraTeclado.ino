625502e56295490328b124325451f1d76f83fcb7
pedroacmendes <pedroacmendes@hotmail.com>
2021-03-06 23:03:07 +0000

    project with buzzer in ecraTeclado

--- ecraTeclado/ecraTeclado.ino
+++ ecraTeclado/ecraTeclado.ino
@@ -1,9 +1,18 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+int GPActiveBuzzer = 13;
+
 LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
 
 void setup() {
+    // Buzzer   
+    pinMode (GPActiveBuzzer, OUTPUT);
+
+    digitalWrite (GPActiveBuzzer, HIGH);
+    delay (2000);
+    digitalWrite (GPActiveBuzzer, LOW);
+  
     //Pinos ligados aos pinos 1, 2, 3 e 4 do teclado - Linhas
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
