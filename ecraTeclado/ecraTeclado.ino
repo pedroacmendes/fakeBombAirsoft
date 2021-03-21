990df2f0431660e6f02e586b47609afc7d9da6d8
pedroacmendes <pedroacmendes@hotmail.com>
2021-03-21 11:58:13 +0000

    update more efficient

--- ecraTeclado/ecraTeclado.ino
+++ ecraTeclado/ecraTeclado.ino
@@ -1,77 +1,41 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
-int GPActiveBuzzer = 13;
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+    {'1','2','3','A'},
+    {'4','5','6','B'},
+    {'7','8','9','C'},
+    {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {9,8,7,6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5,4,3,2}; //connect to the column pinouts of the keypad
 
 LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
 
-void setup() {
-    // Buzzer   
-    pinMode (GPActiveBuzzer, OUTPUT);
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-    digitalWrite (GPActiveBuzzer, HIGH);
-    delay (2000);
-    digitalWrite (GPActiveBuzzer, LOW);
-  
-    //Pinos ligados aos pinos 1, 2, 3 e 4 do teclado - Linhas
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-
-    //Pinos ligados aos pinos 5, 6, 7 e 8 do teclado - Colunas
-    pinMode(8, INPUT);
-    pinMode(9, INPUT);
-    pinMode(10, INPUT);
-    pinMode(11, INPUT);
+void setup(){
+    Serial.begin(9600);
 
     lcd.begin (16,2);
     lcd.setBacklight(HIGH);
-    lcd.print("Pressione uma tecla...");
+    lcd.print("Digite para aparecer no ecra.");
 }
 
-void loop() {
-    for (int ti = 3; ti<7; ti++) { //Alterna o estado dos pinos das linhas
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        digitalWrite(ti, HIGH);
-
-        //Verifica se alguma tecla da coluna 1 foi pressionada
-        if (digitalRead(8) == HIGH) {
-            imprime_linha_coluna(ti-2, 1);
-            while(digitalRead(8) == HIGH){}
-        }
-
-        //Verifica se alguma tecla da coluna 2 foi pressionada
-        if (digitalRead(9) == HIGH) {
-            imprime_linha_coluna(ti-2, 2);
-            while(digitalRead(9) == HIGH){};
-        }
-
-        //Verifica se alguma tecla da coluna 3 foi pressionada
-        if (digitalRead(10) == HIGH) {
-            imprime_linha_coluna(ti-2, 3);
-            while(digitalRead(10) == HIGH){}
-        }
-
-        //Verifica se alguma tecla da coluna 4 foi pressionada
-        if (digitalRead(11) == HIGH) {
-            imprime_linha_coluna(ti-2, 4);
-            while(digitalRead(11) == HIGH){}
-        }
-
+void loop(){
+    char key = keypad.getKey();
+    // just print the pressed key
+    if (key){
+        lcd.clear();
+        lcd.print(key);
     }
-    delay(10);
-}
 
-void imprime_linha_coluna(int x, int y) {
-    lcd.clear();
-    lcd.print("Linha : ");
-    lcd.print(x);
-    lcd.setCursor(0,1);
-    lcd.print("Coluna: ");
-    lcd.print(y);
-    delay(10);
+    // this checkes if 4 is pressed, then do something. Here  we print the text but you can control something.
+    if (key =='4'){
+        lcd.clear();
+        lcd.print("Key 4 is pressed");
+    }
 }
