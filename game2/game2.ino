db18e2da8afe58d8d776ba03e8968309dda8c232
pedroacmendes <pedroacmendes@hotmail.com>
2021-03-29 20:49:36 +0100

    progress bar when defused bomb

--- game2/game2.ino
+++ game2/game2.ino
@@ -18,33 +18,128 @@ LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
 
 int GPActiveBuzzer = 13;
 
-// VARIAVEIS
+// RED & BLUE BUTTON
+int redButtonPin = 11;
+int redButtonState = 0;
+int blueButtonPin = 12;
+int blueButtonState = 0;
+
+// VARIAVEIS COUNTDOWN
 int minutos = 0;
 int segundos = 0;
 long secMillis = 0;
 long interval = 1000;
 int segundosSom = 0;
 
+int led = 10;
+int tempo = 0;
+
+// progressBar
+byte zero[] = {
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000
+};
+byte one[] = {
+    B10000,
+    B10000,
+    B10000,
+    B10000,
+    B10000,
+    B10000,
+    B10000,
+    B10000
+};
+
+byte two[] = {
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000
+};
+byte three[] = {
+    B11100,
+    B11100,
+    B11100,
+    B11100,
+    B11100,
+    B11100,
+    B11100,
+    B11100
+};
+byte four[] = {
+    B11110,
+    B11110,
+    B11110,
+    B11110,
+    B11110,
+    B11110,
+    B11110,
+    B11110
+};
+byte five[] = {
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111
+};
+
+int progress = 50;
+
+// hold button
+int holdTime = 250;
+int lastReading = LOW;
+int hold = 0;
+int LEDstate = 0;
+long onTime = 0;
+
+unsigned long startMillis;
+unsigned long currentMillis;
 
 void setup() {
     lcd.begin (16,4);
     lcd.setBacklight(HIGH);
     pinMode (GPActiveBuzzer, OUTPUT);
+    pinMode(led, OUTPUT);
+    pinMode(redButtonPin, INPUT);
+    pinMode(blueButtonPin, INPUT);
 
-    lcd.print("Game mode:");
+    lcd.createChar(0, zero);
+    lcd.createChar(1, one);
+    lcd.createChar(2, two);
+    lcd.createChar(3, three);
+    lcd.createChar(4, four);
+    lcd.createChar(5, five);
+
+    lcd.setCursor(1,0);
+    lcd.print("Countdown Game:");
     lcd.setCursor(2, 1);
-    lcd.print("A = Countdown");
+    lcd.print("BLUE = Start Game");
     lcd.setCursor(6, 2);
-    lcd.print("C = Defuse Bomb");
+    lcd.print("RED = Defused Bomb");
 }
 
 void loop() {
-
     char key = keypad.getKey();
 
-    if(key == 'A'){
+    blueButtonState = digitalRead(blueButtonPin);
+    redButtonState = digitalRead(redButtonPin);
+
+    if (blueButtonState != HIGH) {
         lcd.clear();
-        lcd.print("Set game time!");
+        lcd.print("Set game time");
         lcd.setCursor(0, 1);
         lcd.print("00:00");
 
@@ -66,6 +161,14 @@ void loop() {
         lcd.print(minutos);
 
         lcd.clear();
+        lcd.print("Starting game in");
+        for (int i=3;i > 0; i--){
+            lcd.setCursor(1, 1);
+            lcd.print(i);
+            delay(1000);
+        }
+        lcd.clear();
+        lcd.setCursor(4,0);
         lcd.print("BOMBA ARMADA");
     }
 
@@ -73,15 +176,49 @@ void loop() {
         countdown();
     }
 
-    // FALTA FAZER O TEMPO APARECER NO MOMENTO QUE CARREGO NAS TECLAS
+    int reading = digitalRead(redButtonPin);
 
-    if(key == 'C'){
-        minutos = 0;
-        segundos = 0;
+    startMillis = millis();
+
+    if(reading == 0){
         lcd.clear();
-        lcd.print("Bomb Defused");
+        lcd.print("A desarmar bomba...");
+        digitalWrite (GPActiveBuzzer, HIGH);
+        digitalWrite(led, HIGH);
+
+        updateProgressBar(progress, 100);
+        progress++;
+
+        delay(300);
+
+        bombdefused();
     }
 
+}
+
+void bombdefused(){
+    digitalWrite(led, LOW);
+    digitalWrite (GPActiveBuzzer, LOW);
+    minutos = 0;
+    segundos = 0;
+    lcd.clear();
+    lcd.print("Bomb Defused");
+}
+
+void updateProgressBar(unsigned long count, unsigned long totalCount){
+    double factor = totalCount/80.0;
+    int percent = (count+1)/factor;
+    int number = percent/5;
+    int j = 1;
+
+    if(number > 0){
+        while(j < 19){
+            lcd.setCursor(j,1);
+            delay(600);
+            lcd.write(5);
+            j++;
+        }
+    }
 }
 
 int GetNumber(){
@@ -112,21 +249,21 @@ int GetNumber(){
 }
 
 void countdown() {
-    lcd.setCursor (3,1);
+    lcd.setCursor (11,2);
     if (minutos < 10){
         lcd.write ("0");
-        lcd.setCursor (4,1);
+        lcd.setCursor (12,2);
     }
 
     lcd.print (minutos);
-    lcd.setCursor (5,1);
+    lcd.setCursor (13,2);
     lcd.print (":");
 
     if(segundos > 9){
-        lcd.setCursor(6, 1);
+        lcd.setCursor(14, 2);
     } else {
         lcd.print("0");
-        lcd.setCursor(7, 1);
+        lcd.setCursor(15, 2);
     }
     lcd.print(segundos);
 
@@ -141,8 +278,10 @@ void countdown() {
 
     if(segundosSom >= 5){
         digitalWrite (GPActiveBuzzer, HIGH);
+        digitalWrite(led, HIGH);
     } else {
         digitalWrite (GPActiveBuzzer, LOW);
+        digitalWrite(led, LOW);
     }
 
     if(segundosSom == 6){
@@ -162,7 +301,8 @@ void countdown() {
     if(minutos == 0 && segundos <= 0){
         lcd.clear();
         lcd.print("The Bomb Has Exploded!");
-        while(1);
+        segundosSom = 0;
+        delay(1000);
     }
 
 }
